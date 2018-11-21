ca6bd6e945fdf5c632ac21ce500e1da71a0e74e6
jonmircha <jonmircha@gmail.com>
2018-11-20 19:49:03 -0600

    Clase 2 Foundation

--- js/foundation.js
+++ js/foundation.js
@@ -1,4 +1,13 @@
 ;
 ((d, w) => {
   $(d).foundation()
+
+  d.addEventListener('DOMContentLoaded', e => {
+    const mqDesktop = w.matchMedia('(min-width: 64em)')
+    $('.top-bar a').click(() => {
+      if (!mqDesktop.matches) {
+        $('.top-bar').hide()
+      }
+    })
+  })
 })(document, window);
