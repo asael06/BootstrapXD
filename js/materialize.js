3d4984f7d15923699f53a8899a9cd2d15358819e
jonmircha <jonmircha@gmail.com>
2018-11-27 19:46:31 -0600

    Clase 2 Materialize CSS

--- js/materialize.js
+++ js/materialize.js
@@ -2,5 +2,16 @@
 ((d) => {
   d.addEventListener('DOMContentLoaded', e => {
     M.AutoInit()
+
+    const sliders = d.querySelectorAll('.slider')
+
+    M.Slider.init(sliders, {
+      indicators: true,
+      duration: 1000
+    })
+
+    const copy = d.querySelector('.Footer small')
+
+    copy.innerHTML = `&copy; ${new Date().getFullYear()} @jonmircha`
   })
 })(document);
